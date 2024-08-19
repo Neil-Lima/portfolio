1ee24d7b063186542d6055963382decb5c50debd
Neil-Lima <110132286+Neil-Lima@users.noreply.github.com>
2024-08-19 09:25:45 -0300

    j

--- backend/services/contactServices.js
+++ backend/services/contactServices.js
@@ -4,11 +4,15 @@ const transporter = nodemailer.createTransport({
   host: "smtp-mail.outlook.com",
   port: 587,
   secure: false,
+  tls: {
+    ciphers: 'SSLv3'
+  },
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
   }
 });
+
 
 exports.sendEmail = async (name, email, subject, message) => {
   const mailOptions = {
